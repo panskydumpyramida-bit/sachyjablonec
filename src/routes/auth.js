f6f358fa426ef7fcbcea913e44dec0d90f1a6fd2
Antonin Duda <antoninduda@Mac.home>
2025-12-04 20:22:07 +0100

    Add user registration endpoint

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { login, me } from '../controllers/authController.js';
+import { login, me, register } from '../controllers/authController.js';
 import { authMiddleware } from '../middleware/auth.js';
 
 const router = express.Router();
 
+router.post('/register', register);
 router.post('/login', login);
 router.get('/me', authMiddleware, me);
 
 export default router;
+
