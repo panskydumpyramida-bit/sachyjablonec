52f8d9f87697b55523624f178e935d44ee008210
Antonin Duda <antoninduda@Antonin-MacBook-Air.local>
2025-12-11 02:03:30 +0100

    Fix: Add fallback password prompt and allow X-Club-Password header

--- src/server.js
+++ src/server.js
@@ -595,7 +595,7 @@ app.use(cors({
     origin: true, // Allow all origins
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Club-Password']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
