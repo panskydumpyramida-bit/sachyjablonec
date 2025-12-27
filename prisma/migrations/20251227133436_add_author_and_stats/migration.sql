dbbf5d940ee42596f72ac243c098b6feae172aa3
Antonin Duda <antoninduda@Mac.home>
2025-12-27 14:35:06 +0100

    fix: force add migrations for deployment

---
+++ prisma/migrations/20251227133436_add_author_and_stats/migration.sql
@@ -0,0 +1,67 @@
+-- AlterEnum
+ALTER TYPE "Role" ADD VALUE 'MEMBER';
+
+-- DropIndex
+DROP INDEX "comments_author_id_idx";
+
+-- DropIndex
+DROP INDEX "comments_news_id_idx";
+
+-- AlterTable
+ALTER TABLE "Message" ADD COLUMN     "type" TEXT NOT NULL DEFAULT 'chat';
+
+-- AlterTable
+ALTER TABLE "news" ADD COLUMN     "author_name" TEXT,
+ADD COLUMN     "co_author_id" INTEGER,
+ADD COLUMN     "co_author_name" TEXT,
+ADD COLUMN     "view_count" INTEGER NOT NULL DEFAULT 0;
+
+-- CreateTable
+CREATE TABLE "chess_games" (
+    "id" SERIAL NOT NULL,
+    "event" TEXT,
+    "site" TEXT,
+    "date" TIMESTAMP(3),
+    "round" TEXT,
+    "white_player" TEXT NOT NULL,
+    "black_player" TEXT NOT NULL,
+    "result" TEXT NOT NULL,
+    "eco" TEXT,
+    "white_elo" INTEGER,
+    "black_elo" INTEGER,
+    "ply_count" INTEGER,
+    "moves" TEXT NOT NULL,
+
+    CONSTRAINT "chess_games_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "chess_moves" (
+    "id" SERIAL NOT NULL,
+    "game_id" INTEGER NOT NULL,
+    "ply_num" INTEGER NOT NULL,
+    "san" TEXT NOT NULL,
+
+    CONSTRAINT "chess_moves_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "chess_games_white_player_idx" ON "chess_games"("white_player");
+
+-- CreateIndex
+CREATE INDEX "chess_games_black_player_idx" ON "chess_games"("black_player");
+
+-- CreateIndex
+CREATE INDEX "chess_games_eco_idx" ON "chess_games"("eco");
+
+-- CreateIndex
+CREATE INDEX "chess_games_date_idx" ON "chess_games"("date");
+
+-- CreateIndex
+CREATE INDEX "chess_moves_san_ply_num_idx" ON "chess_moves"("san", "ply_num");
+
+-- CreateIndex
+CREATE INDEX "chess_moves_game_id_idx" ON "chess_moves"("game_id");
+
+-- AddForeignKey
+ALTER TABLE "news" ADD CONSTRAINT "news_co_author_id_fkey" FOREIGN KEY ("co_author_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
